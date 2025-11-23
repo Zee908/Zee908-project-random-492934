c8e53ad964fbc624ea957ac77d78a1dce747f95f
Zee <amirabaszarkhah268@gmail.com>
2025-11-23 20:31:06 +0330

    update

--- Note/views.py
+++ Note/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Note
 # Create your views here.
 def Note_list(request):
+    context = {
+        'note_list':Note.objects.all()
+    }
     return render(request,'index.html',{})
