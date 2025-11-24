aaea58ab260fa47f8a1a6b49faf2ee2a9098ad25
Zee <amirabaszarkhah268@gmail.com>
2025-11-24 22:47:50 +0330

    update

--- Note/form.py
+++ Note/form.py
@@ -1,5 +1,5 @@
 from django import forms
-class Contact(forms.From):
+class ContactForm(forms.From):
     full_name=forms.CharField(max_length=50,required=True)
     phone_number=forms.CharField(max_length=11,required=True)
     email= forms.EmailField(required=False)
--- Note/views.py
+++ Note/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Note
+from .form  import Contact
 # Create your views here.
 def Note_list(request):
     context = {
